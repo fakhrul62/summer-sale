a0986f3cc7d780f88807cd7042e12e4856e51606
Md. Fakhrul Alam Shuvo <ifakhrul23@gmail.com>
2024-05-04 15:04:56 +0600

    first push

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    daisyui: {
+        themes: ["light", "dark", "cupcake"],
+      },
+      
+    plugins: [],
+    fontFamily: {
+        'body': ['"Work Sans"'],
+      },
+  }
